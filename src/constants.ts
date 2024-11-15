bec6ed12c116f0a1381bbe9dc991755f0cc9d238
root <malishevstanislav@gmail.com>
2024-11-15 12:01:34 +0300

    fix constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000'; // TODO Update with your API URL
-export const BASE_IMG_URL = 'http://80.78.242.165:80/images/'; // TODO Update with your API URL
+export const API_URL = 'https://lazy.moscow:3000'; // TODO Update with your API URL
+export const BASE_IMG_URL = ''; // TODO Update with your API URL
