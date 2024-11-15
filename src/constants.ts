ee717816bc7817543981dadcdb30cf26f12d2b1b
smalyshev <Stanisl.Malyshev@x5.ru>
2024-11-15 05:12:39 +0300

    add photo to public

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
 export const API_URL = 'http://localhost:3000'; // TODO Update with your API URL
-export const BASE_IMG_URL = 'http://80.78.242.165:80/images/'; // TODO Update with your API URL
+// export const BASE_IMG_URL = 'http://80.78.242.165:80/images/'; // TODO Update with your API URL
+// export const BASE_IMG_URL = 'http://www.lazy.moscow:3000/'; // TODO Update with your API URL
+// export const BASE_IMG_URL = 'https://vk.com/app52170852/'; // TODO Update with your API URL
+export const BASE_IMG_URL = ''; // TODO Update with your API URL
