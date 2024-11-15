126b16b6b6192853b770648bf7cba2502a3173eb
smalyshev <Stanisl.Malyshev@x5.ru>
2024-11-15 05:41:00 +0300

    img fix

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const API_URL = 'http://localhost:3000'; // TODO Update with your API URL
 // export const BASE_IMG_URL = 'http://80.78.242.165:80/images/'; // TODO Update with your API URL
 // export const BASE_IMG_URL = 'http://www.lazy.moscow:3000/'; // TODO Update with your API URL
 // export const BASE_IMG_URL = 'https://vk.com/app52170852/'; // TODO Update with your API URL
-export const BASE_IMG_URL = ''; // TODO Update with your API URL
+export const BASE_IMG_URL = 'public/'; // TODO Update with your API URL
