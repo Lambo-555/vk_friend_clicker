d0528d9a09fe46ec332912f5500bde6563f2939e
root <malishevstanislav@gmail.com>
2024-11-17 03:48:48 +0300

    fix image names

--- src/panels/images.ts
+++ src/panels/images.ts
@@ -20,27 +20,27 @@ import photo_2_8 from '../assets/2/8.png';
 import photo_2_9 from '../assets/2/9.png';
 import photo_2_10 from '../assets/2/10.png';
 
-import photo_3_1 from '../assets/2/1.png';
-import photo_3_2 from '../assets/2/2.png';
-import photo_3_3 from '../assets/2/3.png';
-import photo_3_4 from '../assets/2/4.png';
-import photo_3_5 from '../assets/2/5.png';
-import photo_3_6 from '../assets/2/6.png';
-import photo_3_7 from '../assets/2/7.png';
-import photo_3_8 from '../assets/2/8.png';
-import photo_3_9 from '../assets/2/9.png';
-import photo_3_10 from '../assets/2/10.png';
+import photo_3_1 from '../assets/3/1.png';
+import photo_3_2 from '../assets/3/2.png';
+import photo_3_3 from '../assets/3/3.png';
+import photo_3_4 from '../assets/3/4.png';
+import photo_3_5 from '../assets/3/5.png';
+import photo_3_6 from '../assets/3/6.png';
+import photo_3_7 from '../assets/3/7.png';
+import photo_3_8 from '../assets/3/8.png';
+import photo_3_9 from '../assets/3/9.png';
+import photo_3_10 from '../assets/3/10.png';
 
-import photo_4_1 from '../assets/2/1.png';
-import photo_4_2 from '../assets/2/2.png';
-import photo_4_3 from '../assets/2/3.png';
-import photo_4_4 from '../assets/2/4.png';
-import photo_4_5 from '../assets/2/5.png';
-import photo_4_6 from '../assets/2/6.png';
-import photo_4_7 from '../assets/2/7.png';
-import photo_4_8 from '../assets/2/8.png';
-import photo_4_9 from '../assets/2/9.png';
-import photo_4_10 from '../assets/2/10.png';
+import photo_4_1 from '../assets/4/1.png';
+import photo_4_2 from '../assets/4/2.png';
+import photo_4_3 from '../assets/4/3.png';
+import photo_4_4 from '../assets/4/4.png';
+import photo_4_5 from '../assets/4/5.png';
+import photo_4_6 from '../assets/4/6.png';
+import photo_4_7 from '../assets/4/7.png';
+import photo_4_8 from '../assets/4/8.png';
+import photo_4_9 from '../assets/4/9.png';
+import photo_4_10 from '../assets/4/10.png';
 
 export const getCarImageById = (carId: number, imgId: number): string => {
     const images: { [key: string]: { [key: string]: any } } = {
