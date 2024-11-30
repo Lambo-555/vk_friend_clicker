2cbcc11ec8ce5cf78ede32f56fd7d2622f3206aa
smalyshev <Stanisl.Malyshev@x5.ru>
2024-11-30 14:28:32 +0300

    add image links to files

--- src/panels/images.ts
+++ src/panels/images.ts
@@ -53,6 +53,39 @@ import photo_5_8 from '../assets/5/8.png';
 import photo_5_9 from '../assets/5/9.png';
 import photo_5_10 from '../assets/5/10.png';
 
+import photo_6_1 from '../assets/6/1.png';
+import photo_6_2 from '../assets/6/2.png';
+import photo_6_3 from '../assets/6/3.png';
+import photo_6_4 from '../assets/6/4.png';
+import photo_6_5 from '../assets/6/5.png';
+import photo_6_6 from '../assets/6/6.png';
+import photo_6_7 from '../assets/6/7.png';
+import photo_6_8 from '../assets/6/8.png';
+import photo_6_9 from '../assets/6/9.png';
+import photo_6_10 from '../assets/6/10.png';
+
+import photo_7_1 from '../assets/7/1.png';
+import photo_7_2 from '../assets/7/2.png';
+import photo_7_3 from '../assets/7/3.png';
+import photo_7_4 from '../assets/7/4.png';
+import photo_7_5 from '../assets/7/5.png';
+import photo_7_6 from '../assets/7/6.png';
+import photo_7_7 from '../assets/7/7.png';
+import photo_7_8 from '../assets/7/8.png';
+import photo_7_9 from '../assets/7/9.png';
+import photo_7_10 from '../assets/7/10.png';
+
+import photo_8_1 from '../assets/8/1.png';
+import photo_8_2 from '../assets/8/2.png';
+import photo_8_3 from '../assets/8/3.png';
+import photo_8_4 from '../assets/8/4.png';
+import photo_8_5 from '../assets/8/5.png';
+import photo_8_6 from '../assets/8/6.png';
+import photo_8_7 from '../assets/8/7.png';
+import photo_8_8 from '../assets/8/8.png';
+import photo_8_9 from '../assets/8/9.png';
+import photo_8_10 from '../assets/8/10.png';
+
 import hummer_1_1 from '../assets/hummers/1/1.png';
 import hummer_1_2 from '../assets/hummers/1/2.png';
 import hummer_2_1 from '../assets/hummers/2/1.png';
@@ -119,6 +152,42 @@ export const getCarImageById = (carId: number, imgId: number): string => {
             8: photo_5_8,
             9: photo_5_9,
             10: photo_5_10,
+        },
+        6: {
+            1: photo_6_1,
+            2: photo_6_2,
+            3: photo_6_3,
+            4: photo_6_4,
+            5: photo_6_5,
+            6: photo_6_6,
+            7: photo_6_7,
+            8: photo_6_8,
+            9: photo_6_9,
+            10: photo_6_10,
+        },
+        7: {
+            1: photo_7_1,
+            2: photo_7_2,
+            3: photo_7_3,
+            4: photo_7_4,
+            5: photo_7_5,
+            6: photo_7_6,
+            7: photo_7_7,
+            8: photo_7_8,
+            9: photo_7_9,
+            10: photo_7_10,
+        },
+        8: {
+            1: photo_8_1,
+            2: photo_8_2,
+            3: photo_8_3,
+            4: photo_8_4,
+            5: photo_8_5,
+            6: photo_8_6,
+            7: photo_8_7,
+            8: photo_8_8,
+            9: photo_8_9,
+            10: photo_8_10,
         }
     }
     return images?.[carId]?.[imgId];
